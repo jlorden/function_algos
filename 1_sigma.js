6aee0fa9cc611a3c84bbcf6b9f1a760aab80738c
Jlorden <Jamie.lorden@gmail.com>
2020-02-20 11:55:05 -0600

    function

--- 1_sigma.js
+++ 1_sigma.js
@@ -2,7 +2,12 @@
 // Predcted output: sigma(5) should return 15   (I.E: 1+2+3+4+5 =15)
 
 function sigma(num){
-    // YOUR CODE HERE
+    var sum = 0;
+    for(var i = 0; i <= num; i++){
+        sum = sum + i;
+    }
+    return(sum);
 }
+console.log("sigma(5)");
 var sum = sigma(5);
 console.log(sum);
--- 2_greater_than_y.js
+++ 2_greater_than_y.js
@@ -2,7 +2,13 @@
 // Predcted output: greaterThanY([1,6,8,3,5,2], 4) should return 3
 
 function greaterThanY(arr, y){
-    // YOUR CODE HERE
+    var count = 0;
+    for (var i = 0; i <= arr.length; i++){
+        if ( arr[i] > y){
+        count = count + 1;
+        }
+    }
+    return(count)
 }
 var result = greaterThanY([1,6,8,3,5,2], 4);
 console.log(result);
